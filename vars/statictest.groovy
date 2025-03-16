11cc5b3375191e529f652681d36d5ffc1269060f
mohitmaikhuri03 <mkmaikhuri@gmail.com>
2025-03-16 14:01:53 +0530

    Update statictest.groovy

--- vars/statictest.groovy
+++ vars/statictest.groovy
@@ -1,4 +1,4 @@
-def call(String projectKey, String sonar, String credentialsId ) {
+def call(String projectKey, String sonar, String credentialsId = 'sonar' ) {
     withSonarQubeEnv(sonar) { 
         withCredentials([string(credentialsId: credentialsId, variable: 'SONARQUBE_TOKEN')]) {
             sh """
@@ -6,8 +6,8 @@ def call(String projectKey, String sonar, String credentialsId ) {
             -Dsonar.projectKey=${projectKey} \
             -Dsonar.login=${SONARQUBE_TOKEN} \
             -Dsonar.projectName='${projectKey}' \
-            // -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml \
-            -Dsonar.qualitygate.wait=true 
+            # -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml \
+            # -Dsonar.qualitygate.wait=true 
             """
         }
     }
