7d47722306d5b2e2072f74fd55c0e42b38ab80a4
mohitmaikhuri03 <mkmaikhuri@gmail.com>
2025-03-16 14:13:22 +0530

    Update statictest.groovy

--- vars/statictest.groovy
+++ vars/statictest.groovy
@@ -2,7 +2,7 @@ def call(String projectKey, String credentialsId ) {
     withSonarQubeEnv('sonar') { 
         withCredentials([string(credentialsId: credentialsId, variable: 'SONARQUBE_TOKEN')]) {
             sh """
-            mvn sonar:sonar \
+            mvn clean verify sonar:sonar \
             -Dsonar.projectKey=${projectKey} \
             -Dsonar.login=${SONARQUBE_TOKEN} \
             -Dsonar.projectName='${projectKey}' \
