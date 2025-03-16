6ed400a242b773cef7461874b000c329e63d898e
mohitmaikhuri03 <mkmaikhuri@gmail.com>
2025-03-16 13:45:09 +0530

    Update passnotify.groovy

--- vars/passnotify.groovy
+++ vars/passnotify.groovy
@@ -9,7 +9,7 @@ def call(String emailRecipient, String slackChannel,String slackTokenId) {
             slackSend(
                 channel: slackChannel, 
                 message: "Build Successful: JOB-Name:- ${env.JOB_NAME} Build_No.:- ${env.BUILD_NUMBER} & Build-URL:- ${env.BUILD_URL}", 
-                tokenCredentialId: 'slackTokenId'
+                tokenCredentialId: slackTokenId
             )
         }
     }
