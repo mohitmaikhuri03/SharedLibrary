96121a418573417fcba6be9b6f4f88bad29d5ee0
mohitmaikhuri03 <mkmaikhuri@gmail.com>
2025-03-16 14:57:00 +0530

    Update passnotify.groovy

--- vars/passnotify.groovy
+++ vars/passnotify.groovy
@@ -1,16 +1,12 @@
 def call(String emailRecipient, String slackChannel, String slackTokenId) {
-    post {
-        success {
-            emailext(
-                body: 'Build Success', 
-                subject: "Job Name: ${env.JOB_NAME}, Build Number: ${env.BUILD_NUMBER}", 
-                to: emailRecipient
-            )
-            slackSend(
-                channel: slackChannel, 
-                message: "Build Successful: JOB-Name:- ${env.JOB_NAME} Build_No.:- ${env.BUILD_NUMBER} & Build-URL:- ${env.BUILD_URL}", 
-                tokenCredentialId: slackTokenId
-            )
-        }
-    }
+    emailext(
+        body: 'Build Success', 
+        subject: "Job Name: ${env.JOB_NAME}, Build Number: ${env.BUILD_NUMBER}", 
+        to: emailRecipient
+    )
+    slackSend(
+        channel: slackChannel, 
+        message: "Build Successful: JOB-Name:- ${env.JOB_NAME} Build_No.:- ${env.BUILD_NUMBER} & Build-URL:- ${env.BUILD_URL}", 
+        tokenCredentialId: slackTokenId
+    )
 }
