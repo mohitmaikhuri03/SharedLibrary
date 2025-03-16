08cc3965398a4ecd05c94ca4dd0349bbd1f7d3d2
mohitmaikhuri03 <mkmaikhuri@gmail.com>
2025-03-16 14:57:25 +0530

    Update failnotify.groovy

--- vars/failnotify.groovy
+++ vars/failnotify.groovy
@@ -1,16 +1,13 @@
 def call(String emailRecipient, String slackChannel, String slackTokenId) {
-    post {
-        success {
-            emailext(
-                body: 'Build failure', 
-                subject: "Job Name: ${env.JOB_NAME}, Build Number: ${env.BUILD_NUMBER}", 
-                to: emailRecipient
-            )
-            slackSend(
-                channel: slackChannel, 
-                message: "Build Failure: JOB-Name:- ${env.JOB_NAME} Build_No.:- ${env.BUILD_NUMBER} & Build-URL:- ${env.BUILD_URL}", 
-                tokenCredentialId: slackTokenId
-            )
-        }
-    }
+    emailext(
+        body: 'Build Failure', 
+        subject: "Job Name: ${env.JOB_NAME}, Build Number: ${env.BUILD_NUMBER}", 
+        to: emailRecipient
+    )
+    slackSend(
+        channel: slackChannel, 
+        message: "Build Failure: JOB-Name:- ${env.JOB_NAME} Build_No.:- ${env.BUILD_NUMBER} & Build-URL:- ${env.BUILD_URL}", 
+        tokenCredentialId: slackTokenId
+    )
 }
+
