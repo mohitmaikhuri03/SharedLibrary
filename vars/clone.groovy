0f2fdc5d8899fe444856ca978c26ef315aa3750f
mohitmaikhuri03 <mkmaikhuri@gmail.com>
2025-03-16 15:04:14 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,3 +1,7 @@
-def call(String branch, String url, String credentialsId) {
-    git branch: branch, url: url  , credentialsId: credentialsId
+def call(String branch, String url, String credentialsId = null) {
+    if (credentialsId) {
+        git branch: branch, url: url, credentialsId: credentialsId
+    } else {
+        git branch: branch, url: url
+    }
 }
